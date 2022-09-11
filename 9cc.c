9c419bcd4b04b1ac04a9c238e5289e0d0a687cc3
Kwkmts <87115759+Kwkmts@users.noreply.github.com>
2022-09-11 23:46:50 +0900

    掛け算や割り算、()を含んだ式に対応

--- 9cc.c
+++ 9cc.c
@@ -5,6 +5,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+//
+//トークナイザー
+//
+
 //トークンの種類
 typedef enum {
     TK_RESERVED,  //記号
@@ -102,22 +106,140 @@ Token *tokenize() {
             continue;
         }
 
-        if (*p == '+' || *p == '-') {
-            cur = new_token(TK_RESERVED, cur, p++);
-            continue;
-        }
-
+        //数値リテラル
         if (isdigit(*p)) {
             cur = new_token(TK_NUM, cur, p);
             cur->val = strtol(p, &p, 10);
             continue;
         }
 
+        //区切り文字
+        if (ispunct(*p)) {
+            cur = new_token(TK_RESERVED, cur, p++);
+            continue;
+        }
+
         error_at(p, "トークナイズできません");
     }
 
-    new_token(TK_EOF, cur, p);
+    cur = cur->next = new_token(TK_EOF, cur, p);
     return head.next;
+}
+
+//
+//パーサー
+//
+
+//抽象構文木のノードの種類
+typedef enum {
+    ND_ADD,  // +
+    ND_SUB,  // -
+    ND_MUL,  // *
+    ND_DIV,  // /
+    ND_NUM,  // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+    NodeKind kind;  //ノードの種類
+    Node *lhs;      //左辺
+    Node *rhs;      //右辺
+    int val;        // kindがND_NUMの場合、その値
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node *mul();
+Node *primary();
+
+// expr = mul ("+" mul | "-" mul)*
+Node *expr() {
+    Node *node = mul();
+
+    for (;;) {
+        if (consume('+'))
+            node = new_node(ND_ADD, node, mul());
+        else if (consume('-'))
+            node = new_node(ND_SUB, node, mul());
+        else
+            return node;
+    }
+}
+
+// mul = primary ("*" primary | "/" primary)*
+Node *mul() {
+    Node *node = primary();
+
+    for (;;) {
+        if (consume('*'))
+            node = new_node(ND_MUL, node, primary());
+        else if (consume('/'))
+            node = new_node(ND_DIV, node, primary());
+        else
+            return node;
+    }
+}
+
+// primary = "(" expr ")" | num
+Node *primary() {
+    //次のトークンが"("なら、"(" expr ")"のはず
+    if (consume('(')) {
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+
+    //そうでなければ数値のはず
+    return new_node_num(expect_number());
+}
+
+//
+//コード生成部
+//
+
+void gen(Node *node) {
+    if (node->kind == ND_NUM) {
+        printf("    push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("    pop rdi\n");
+    printf("    pop rax\n");
+
+    switch (node->kind) {
+        case ND_ADD:
+            printf("    add rax, rdi\n");
+            break;
+        case ND_SUB:
+            printf("    sub rax, rdi\n");
+            break;
+        case ND_MUL:
+            printf("    imul rax, rdi\n");
+            break;
+        case ND_DIV:
+            printf("    cqo\n");
+            printf("    idiv rdi\n");
+            break;
+    }
+
+    printf("    push rax\n");
 }
 
 int main(int argc, char **argv) {
@@ -126,28 +248,21 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    //トークナイズ&パース
     user_input = argv[1];
-    token = tokenize();  //トークナイズ
+    token = tokenize();
+    Node *node = expr();
 
     //アセンブリの前半部分を出力
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
 
-    //式の最初は数でなければならないので、それをチェックして最初のmov命令を出力
-    printf("    mov rax, %d\n", expect_number());
+    //抽象構文木を下りながらコード生成
+    gen(node);
 
-    //`+ <数>`あるいは`- <数>`というトークンの並びを消費しつつアセンブリを出力
-    while (!at_eof()) {
-        if (consume('+')) {
-            printf("    add rax, %d\n", expect_number());
-            continue;
-        }
-
-        expect('-');
-        printf("    sub rax, %d\n", expect_number());
-    }
-
+    //スタックトップに式全体の値が残っているはずなのでそれをRAXにロードして関数からの戻り値とする
+    printf("    pop rax\n");
     printf("    ret\n");
     return 0;
 }
