a3c83926e52e1e259f4915579eb9d3f8584c243c
Kwkmts <87115759+Kwkmts@users.noreply.github.com>
2023-10-10 16:55:46 +0900

    ハッシュマップを実装

--- Makefile
+++ Makefile
@@ -80,7 +80,11 @@ err-test: 9cc
 err-test-stage2: stage2/9cc
 	test/err_test.sh ./$^
 
+hashmap-test:
+	cc -e hashmap_test -o hashmap_test *.c && ./hashmap_test
+	@echo "OK"; echo
+
 clean:
-	$(RM) 9cc $(PREPROCESSED_SRCS) *.o *~ test/*.s test/*.out stage2 stage3
+	$(RM) 9cc hashmap_test $(PREPROCESSED_SRCS) *.o *~ test/*.s test/*.out stage2 stage3
 
 .PHONY: all test-all test test-stage2 cmp-stage23 err-test clean
--- preprocess.c
+++ preprocess.c
@@ -1,5 +1,180 @@
 #include "9cc.h"
 
+//
+// ハッシュマップ(オープンアドレス法)の実装
+//
+
+#define HASHMAP_INIT_SIZE 16
+#define HASHMAP_MAX_LOAD 75
+#define TOMBSTONE ((void *)-1)
+
+typedef struct {
+    char *key;
+    int keylen;
+    void *val;
+} HashmapEntry;
+
+typedef struct Hashmap *Hashmap;
+struct Hashmap {
+    HashmapEntry *buckets;
+    int capacity;
+    int used;
+};
+
+Hashmap hashmap_new() {
+    Hashmap map = calloc(1, sizeof(struct Hashmap));
+    map->capacity = HASHMAP_INIT_SIZE;
+    map->buckets = calloc(HASHMAP_INIT_SIZE, sizeof(HashmapEntry));
+    return map;
+}
+
+static uint64_t fnv_hash(char *key, int keylen) {
+    uint64_t hash = 0xcbf29ce484222325;
+    for (int i = 0; i < keylen; i++) {
+        hash *= 0x100000001b3;
+        hash ^= *key++;
+    }
+    return hash;
+}
+
+void hashmap_put(Hashmap map, char *key, int keylen, void *val);
+
+static void rehash(Hashmap map) {
+    int oldcap = map->capacity;
+    HashmapEntry *oldbuckets = map->buckets;
+
+    map->capacity *= 2;
+    map->buckets = calloc(map->capacity, sizeof(HashmapEntry));
+    map->used = 0;
+
+    for (int i = 0; i < oldcap; i++) {
+        HashmapEntry *e = &oldbuckets[i];
+        if (e->key && e->key != TOMBSTONE) {
+            hashmap_put(map, e->key, e->keylen, e->val);
+        }
+    }
+}
+
+static bool match(HashmapEntry *entry, char *key, int keylen) {
+    return entry->key != TOMBSTONE && entry->keylen == keylen &&
+           !memcmp(entry->key, key, keylen);
+}
+
+static HashmapEntry *get_entry(Hashmap map, char *key, int keylen) {
+    uint64_t hash = fnv_hash(key, keylen);
+
+    for (uint64_t i = hash % map->capacity; i < map->capacity;
+         i = (i + 1) % map->capacity) {
+        HashmapEntry *e = &map->buckets[i];
+        if (!e->key) {
+            return NULL;
+        }
+        if (match(e, key, keylen)) {
+            return e;
+        }
+    }
+
+    unreachable();
+}
+
+static HashmapEntry *get_or_insert_entry(Hashmap map, char *key, int keylen) {
+    if (map->used * 100 / map->capacity >= HASHMAP_MAX_LOAD) {
+        rehash(map);
+    }
+
+    uint64_t hash = fnv_hash(key, keylen);
+
+    for (uint64_t i = hash % map->capacity;; i = (i + 1) % map->capacity) {
+        HashmapEntry *e = &map->buckets[i];
+        if (!e->key) {
+            map->used++;
+            e->key = key;
+            e->keylen = keylen;
+            return e;
+        }
+        if (e->key == TOMBSTONE) {
+            e->key = key;
+            e->keylen = keylen;
+            return e;
+        }
+        if (match(e, key, keylen)) {
+            return e;
+        }
+    }
+}
+
+void *hashmap_get(Hashmap map, char *key, int keylen) {
+    HashmapEntry *e = get_entry(map, key, keylen);
+    return e ? e->val : NULL;
+}
+
+void hashmap_put(Hashmap map, char *key, int keylen, void *val) {
+    HashmapEntry *e = get_or_insert_entry(map, key, keylen);
+    e->val = val;
+}
+
+void hashmap_delete(Hashmap map, char *key, int keylen) {
+    HashmapEntry *e = get_entry(map, key, keylen);
+    if (e) {
+        e->key = TOMBSTONE;
+    }
+}
+
+int hashmap_test() {
+    Hashmap map = hashmap_new();
+
+    for (int i = 0; i < 5000; i++) {
+        char *key = format("key %d", i);
+        hashmap_put(map, key, (int)strlen(key), (void *)(size_t)i);
+    }
+    for (int i = 1000; i < 2000; i++) {
+        char *key = format("key %d", i);
+        hashmap_delete(map, key, (int)strlen(key));
+    }
+    for (int i = 1500; i < 1600; i++) {
+        char *key = format("key %d", i);
+        hashmap_put(map, key, (int)strlen(key), (void *)(size_t)i);
+    }
+    for (int i = 6000; i < 7000; i++) {
+        char *key = format("key %d", i);
+        hashmap_put(map, key, (int)strlen(key), (void *)(size_t)i);
+    }
+
+    for (int i = 0; i < 1000; i++) {
+        char *key = format("key %d", i);
+        assert((size_t)hashmap_get(map, key, (int)strlen(key)) == i);
+    }
+    for (int i = 1000; i < 1500; i++) {
+        assert(!hashmap_get(map, "no such key", 11));
+    }
+    for (int i = 1500; i < 1600; i++) {
+        char *key = format("key %d", i);
+        assert((size_t)hashmap_get(map, key, (int)strlen(key)) == i);
+    }
+    for (int i = 1600; i < 2000; i++) {
+        assert(!hashmap_get(map, "no such key", 11));
+    }
+    for (int i = 2000; i < 5000; i++) {
+        char *key = format("key %d", i);
+        assert((size_t)hashmap_get(map, key, (int)strlen(key)) == i);
+    }
+    for (int i = 5000; i < 6000; i++) {
+        assert(!hashmap_get(map, "no such key", 11));
+    }
+    for (int i = 6000; i < 7000; i++) {
+        char *key = format("key %d", i);
+        hashmap_put(map, key, (int)strlen(key), (void *)(size_t)i);
+    }
+
+    assert(!hashmap_get(map, "no such key", 11));
+
+    exit(0);
+}
+
+//
+// プリプロセッサ
+//
+
 static Token *token; // 現在着目しているトークン
 
 typedef struct CondIncl CondIncl;
